c31fae95885013bc19190e7b78384c0a0d056443
English_Trini_Wizard <46840116+TrinUkWoLoz@users.noreply.github.com>
2020-04-21 20:30:00 +0100

    Update run_monitor.sh
    Final amendments..

--- run_monitor.sh
+++ run_monitor.sh
@@ -34,7 +34,7 @@ fi
 if [[ ! -z $vopt ]]
 then
 {
-echo -e "LJCH - System & Network Monitor ,\nWritten by W1z4r|)0fL0z_TnT!"
+echo -e "Ubuntu System and Network Monitor Tool ,\nWritten by W1z4r|)0fL0z_TnT!"
 }
 fi
 
